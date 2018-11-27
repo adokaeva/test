2455e8e5cdf576bf32659a0b77efa129fb654b4f
adokaeva@issart.com <adokaeva@issart.com>
2018-11-27 15:26:42 +0600

    GIT-2936:added attributes file in root

---
+++ 100_commits.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+oldname="D:\test\test\test.txt"
+prefix="D:\test\test\test"
+postfix=".txt" 
+for i in {1..100}
+    do
+        newname="$prefix$i$postfix"
+		mv $oldname $newname
+		git add $oldname
+		git add $newname
+		git commit -m TST-2
+		oldname=$newname
+        echo $newname  
+    done
+  
